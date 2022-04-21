0f97bda3cc36d1e4379099921827265795a5b849
rinsoo <rinsoo1994@gmail.com>
2022-04-21 22:20:16 +0900

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# rinsorder
---
+++ src/main/java/com/rinsorder/RinsorderApplication.java
@@ -0,0 +1,13 @@
+package com.rinsorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinsorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RinsorderApplication.class, args);
+	}
+
+}
