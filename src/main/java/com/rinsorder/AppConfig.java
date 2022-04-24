37e26fe7261614423ffb2a7eab8d5242c73fcaac
rinsoo <rinsoo1994@gmail.com>
2022-04-24 14:40:10 +0900

    Add: AppConfig

--- src/main/java/com/rinsorder/AppConfig.java
+++ src/main/java/com/rinsorder/AppConfig.java
@@ -1,6 +1,9 @@
 package com.rinsorder;
 
+import com.rinsorder.discount.DiscountPolicy;
+import com.rinsorder.discount.FixDiscountPolicy;
 import com.rinsorder.discount.RateDiscountPolicy;
+import com.rinsorder.member.MemberRepository;
 import com.rinsorder.member.MemberService;
 import com.rinsorder.member.MemberServiceImpl;
 import com.rinsorder.member.MemoryMemberRepository;
@@ -8,15 +11,21 @@ import com.rinsorder.order.OrderService;
 import com.rinsorder.order.OrderServiceImpl;
 
 public class AppConfig {
+    // OCP, SRP, DIP를 위반하지 않도록 하는 AppConfig
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new RateDiscountPolicy()
-        );
+        return new OrderServiceImpl( memberRepository(), discountPolicy());
     }
 
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
+    }
 }
